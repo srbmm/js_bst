1d9819cd44f4a587e471e9cd5e5c991655042dbb
Mohammad Sohrabi <shrbmamad@gmail.com>
2023-01-28 17:28:29 +0330

    addfunc added.

---
+++ Js_BST.js
@@ -0,0 +1,91 @@
+class BST_Node{
+    constructor(key){
+        this.key = key;
+        this.left = undefined;
+        this.right = undefined;
+    }
+}
+
+class BST{
+    constructor(NodeClass = BST_Node) {
+        this.root = undefined;
+        this.NodeClass = NodeClass;
+        this.temp = "";
+    }
+    _add(root, key, ...args){
+        if(root === undefined){
+            root = new this.NodeClass(key, ...args);
+            return root
+        }else if(root.key >= key){
+            root.left = this._add(root.left, key, ...args);
+        }else if(root.key < key){
+            root.right = this._add(root.right, key, ...args);
+        }
+        return root
+    }
+    add(value, ...args){
+        this.root = this._add(this.root, value, ...args);
+    }
+    #printPostorder(root, index) {
+        if (root === undefined)
+            return;
+        this.#printPostorder(root.left, index);
+        this.#printPostorder(root.right, index);
+        this.temp = this.temp + `${root[index]}, `
+    }
+    postorder(index){
+        this.temp = ""
+        this.#printPostorder(this.root, index);
+        const temp =  this.temp
+        this.temp = ""
+        return temp
+    }
+    #printInorder(root, index) {
+        if (root === undefined)
+            return;
+        this.#printInorder(root.left, index);
+        this.temp = this.temp + `${root[index]}, `
+        this.#printInorder(root.right, index);
+    }
+    inorder(index= "key"){
+        this.temp = ""
+        this.#printInorder(this.root, index);
+        const temp =  this.temp
+        this.temp = ""
+        return temp
+    }
+    #printPreorder(root, index) {
+        if (root === undefined)
+            return;
+        this.temp = this.temp + `${root[index]}, `
+        this.#printPreorder(root.left, index);
+        this.#printPreorder(root.right, index);
+    }
+    preorder(index){
+        this.temp = ""
+        this.#printPreorder(this.root, index);
+        const temp =  this.temp
+        this.temp = ""
+        return temp
+    }
+}
+
+
+// test
+
+class BSTCostumeNode extends BST_Node{
+    constructor(key, value) {
+        super(key);
+        this.value = value
+    }
+}
+const test = new BST(BSTCostumeNode);
+test.add(15, 'ali');
+test.add(7, 'reza');
+test.add(25, 'nima');
+test.add(29, 'hamid');
+test.add(26, 'alireza');
+test.add(24, 'ahmad');
+test.add(10, 'donya');
+test.add(4, 'sohrab');
+console.log(test.preorder("value"));
